b7b7e1359c1b97d99402a8759804dfacaed7b682
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2024-01-27 22:38:25 +0600

    Update PalindromeNumber.java

--- src/PalindromeNumber.java
+++ src/PalindromeNumber.java
@@ -1,10 +1,20 @@
 import java.util.Scanner;
 
 public class PalindromeNumber {
+    public static void palindrome(int n){
+        String val = Integer.toString(n);
+        String rev = "";
+        for (int i=val.length()-1;i>=0;i--){
+            rev = rev+val.charAt(i);
+        }
+        if (val.equals(rev)) System.out.println("Palindrome");
+        else System.out.println("Not Palindrome");
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("enter number");
         int num = sc.nextInt();
+
         int org_num = num;
         int reverse = 0;
         while (num !=0){
@@ -13,5 +23,7 @@ public class PalindromeNumber {
         }
         if (reverse == org_num) System.out.println("palindrome");
         else System.out.println("not palindrome");
+
+        palindrome(num);
     }
 }
