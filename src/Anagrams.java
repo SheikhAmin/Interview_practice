a86868899e64579b9fb1f9abf390b17113a62931
MD.SHEIKH AMIN <sheikhamin.cs@gmail.com>
2024-01-28 11:11:20 +0600

    Update Anagrams.java

--- src/Anagrams.java
+++ src/Anagrams.java
@@ -3,14 +3,17 @@ import java.util.stream.Collectors;
 
 public class Anagrams {
     public static boolean areAnagrams(String s, String s1){
-     char [] chars = s.toCharArray();
-     Arrays.sort(chars);
-     String s2 = new String(chars);
-     char [] chars1 = s1.toCharArray();
-     Arrays.sort(chars1);
-     String s3 = new String(chars1);
-     if (s2.equals(s3)) return true;
-     else return false;
+        String s2=s.toLowerCase();
+        String s3 =s1.toLowerCase();
+        char [] chars = s2.toCharArray();
+
+        Arrays.sort(chars);
+        String s4 = new String(chars);
+        char [] chars1 = s3.toCharArray();
+        Arrays.sort(chars1);
+        String s5 = new String(chars1);
+        if (s4.equals(s5)) return true;
+        else return false;
     }
     public static void main(String[] args) {
         System.out.println(areAnagrams("listen","silent"));
